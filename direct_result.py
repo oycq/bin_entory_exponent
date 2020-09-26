c116d77ba4862d78411296d5e78d717f164ba92d
oycq <490746960@qq.com>
2020-09-26 14:46:07 +0800

    update direct_reuslt.py, use a new methods use union-20, 3 layers: train90.85%, test 89.33%, 1 layers, train 87.4717%, test 87.51%

--- direct_result.py
+++ direct_result.py
@@ -15,11 +15,15 @@ save_npy_name = 'most_similar_0.5_0.005_7500_200_Ltrainset.npy'
 save_npy_name2 = 'most_similar_0.5_0.005_7500_200_Ltrainset_2.npy'
 save_npy_name3 = 'most_similar_0.5_0.005_7500_200_Ltrainset_3.npy'
 
+
+output_save_npy_name = 'direct_output_union_20_layer1.npy'
+
 CLASS = 10
 W_LEN = 200
 CUDA = 1
 CRADLE_SIZE = 50
-REPRO_SIZE = 30
+REPRO_SIZE = 10
+UNION = 20
 
 weights = np.load(save_npy_name)
 weights = torch.from_numpy(weights).cuda()[:W_LEN]
@@ -37,10 +41,10 @@ images_t,labels_t = dl_test.get_all()
 
 images = get_images_output(weights,images)
 images_t = get_images_output(weights,images_t)
-images = get_images_output(weights2,images)
-images_t = get_images_output(weights2,images_t)
-images = get_images_output(weights3,images)
-images_t = get_images_output(weights3,images_t)
+#images = get_images_output(weights2,images)
+#images_t = get_images_output(weights2,images_t)
+#images = get_images_output(weights3,images)
+#images_t = get_images_output(weights3,images_t)
 
 
 
@@ -50,28 +54,50 @@ images_t[images_t == 0] = -1
 images = images.repeat(REPRO_SIZE, 1, 1)
 images_t = images_t.repeat(REPRO_SIZE, 1, 1)
 
-cradle = Cradle(CRADLE_SIZE, CLASS * W_LEN, mutation_rate = 0.005,
+cradle = Cradle(CRADLE_SIZE, CLASS * W_LEN * UNION, mutation_rate = 0.005,
             fading_rate = 0.99995,cuda=CUDA)
-
 
 
 cradle.from_strach()
 best_acc = 0
-for i in range(100000):
-    brunch_w = cradle.get_w(REPRO_SIZE)
-    w = brunch_w.reshape(REPRO_SIZE,W_LEN,CLASS)
+best_acc_t = 0
+
+
+def get_output(images, brunch_w):
+    w = brunch_w.reshape(REPRO_SIZE,W_LEN,CLASS*UNION)
     r = images.bmm(w) 
     r = (r + W_LEN) / 2
-    r = r / (r.sum(2).unsqueeze(2))
+    top_k_elements,_ = torch.topk(r, int(CLASS*UNION*0.2), 2)
+    throat,_ = top_k_elements.min(2)
+    throat = throat.unsqueeze(2)
+    r = (r >= throat).float()
+    r = r.reshape(REPRO_SIZE, -1, CLASS, UNION)
+    r = r.sum(3)
+    r = r.exp()
+    r = r/(r.sum(2).unsqueeze(2))
+    return r 
+
+
+for i in range(100000):
+    brunch_w = cradle.get_w(REPRO_SIZE)
+    r = get_output(images, brunch_w)
     r = get_loss(r, labels)
     cradle.pk(brunch_w,r)
     if i % 100 == 0:
-        w = cradle.get_best()[0]
-        w = brunch_w.reshape(REPRO_SIZE,W_LEN,CLASS)
-        r = images_t.bmm(w) 
-        acc = show_accuarcate(r,labels_t,train = 0)
+        w = cradle.get_best()[1].cpu().numpy()
+        np.save(output_save_npy_name,w)
+
+        w = cradle.get_best()[1].unsqueeze(0).repeat(REPRO_SIZE, 1, 1)
+        r = get_output(images, w)
+        acc = show_accuarcate(r,labels,train = 0)
         if acc > best_acc:
             best_acc = acc
-        print('loss:%8.4f   %8.4f'%(cradle.get_best()[0],best_acc))
+        w = cradle.get_best()[1].unsqueeze(0).repeat(REPRO_SIZE, 1, 1)
+        r = get_output(images_t, w)
+        acc = show_accuarcate(r,labels_t,train = 0)
+        if acc > best_acc_t:
+            best_acc_t = acc
+
+        print('loss:%8.4f   train:%8.4f  test:%8.4f'%(cradle.get_best()[0],best_acc,best_acc_t))
 
        
