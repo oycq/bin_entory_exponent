24ce6a16bf1671cecfa50a94c1674e51f1b6b2cf
oycq <490746960@qq.com>
2020-10-01 21:47:00 +0800

    add test_acc function

--- five.py
+++ five.py
@@ -13,25 +13,28 @@ random.seed(0)
 np.random.seed(0)
 torch.manual_seed(0)
 
+IF_SAVE = 1
+SAVE_NAME = 'five_v1.npy'
 INPUT_SIZE = 784
-DIM_RATE = 0.995 #0.995^100=0.60577
-ITERATION = 1000
+ITERATION = 784 
 TOP_K_RATE = 0.005
 
 
 dl = DataLoader(True,cuda = 1)
 images,labels = dl.get_all()
-images, labels = images[:1000],labels[:1000]
+images, labels = images[:10000],labels[:10000]
 dl_test = DataLoader(False,cuda = 1)
 images_t,labels_t = dl_test.get_all()
+images_t, labels_t = images_t,labels_t
 
 
 accumulate = torch.zeros((labels.shape[0],labels.shape[0]),dtype = torch.float32)
 accumulate -= 9999 * torch.eye(labels.shape[0])
-accumulate_t = torch.zeros((labels_t.shape[0],labels.shape[0]),dtype = torch.float32)
+accumulate_t = torch.zeros((labels_t.shape[0],labels_t.shape[0]),dtype = torch.float32)
 
 accumulate = accumulate.cuda()
 accumulate_t = accumulate_t.cuda()
+saved_data = np.zeros((ITERATION,5,2),dtype = int)
 
 
 def get_similarity_table(o1,o2):
@@ -65,21 +68,17 @@ def get_loss(class_s_table, labels):
     r = r.mean()
     return r
 
-def show_accuarcate(r, labels, detail=False):#r:classfication_score_table
+def show_accuarcate(r, labels, train=True):#r:classfication_score_table
     a = torch.argmax(r,1)
     b = labels.argmax(1)
     accuarcate = torch.mean((a==b).float())*100
-    if detail:
-        print('Train accuarcate:%6.2f%%'%(accuarcate))
-        print(r[:5,:9])
-        print(labels[:5])
+    if train:
+        print('Train accuarcate:%6.2f%%\n\n'%(accuarcate))
+        #print(r[:5,:9])
+        #print(labels[:5])
     else:
         print('Test accuarcate:%6.2f%%\n\n'%(accuarcate))
     return accuarcate
-
-combine_input = images.clone()
-combine_input_t = images_t.clone()
-
 
 
 if __name__ == '__main__':
@@ -89,12 +88,12 @@ if __name__ == '__main__':
         avoid_repeat_list = []
         for f in range(5):
             best = {'column':0, 'bit_w':0, 'loss':9999, 'o':None}
-            for column in tqdm(range(combine_input.shape[1]), leave=False):
+            for column in tqdm(range(images.shape[1]), leave=False):
                 if column in avoid_repeat_list:
                     continue
                 for bit_w in [-1,1]:
-                    rand_swing = torch.randint(0, 2, (combine_input.shape[0],)).cuda().float()
-                    o = out_accumu + bit_w * combine_input[:,column] + rand_swing
+                    rand_swing = torch.randint(0, 2, (images.shape[0],)).cuda().float()
+                    o = out_accumu + bit_w * images[:,column] + rand_swing
                     o = (o >= 1).float().unsqueeze(1)
                     r = get_similarity_table(o,o)
                     r = get_classfication_score_table(r, labels, accumulate)
@@ -104,30 +103,29 @@ if __name__ == '__main__':
                         best['column'] = column
                         best['bit_w'] = bit_w
                         best['o']  = o
+                saved_data[iteration,f,0] = best['column']
+                saved_data[iteration,f,1] = best['bit_w']
             print('%5d %5d   bit_w:%2d  loss:%8.5f'%\
                     (f,best['column'],best['bit_w'],best['loss']))
-            out_accumu += combine_input[:,best['column']] * best['bit_w']
+            out_accumu += images[:,best['column']] * best['bit_w']
             avoid_repeat_list.append(best['column'])
 
         o = (out_accumu > 0).float().unsqueeze(1)
-        r = get_similarity_table(o,o)
-        accumulate += r
-        r = get_classfication_score_table(r, labels, accumulate)
+        similar_table = get_similarity_table(o,o)
+        r = get_classfication_score_table(similar_table, labels, accumulate)
+        accumulate = accumulate + similar_table 
         show_gather(best['o'],labels)
         show_accuarcate(r, labels)
 
+        test_o = 0
+        for i in range(5):
+            test_o += images_t[:,saved_data[iteration, i, 0]] * saved_data[iteration,i,1]
+        test_o = (test_o > 0).float().unsqueeze(1)
+        similar_table_t = get_similarity_table(test_o,test_o)
+        r = get_classfication_score_table(similar_table_t, labels_t, accumulate_t)
+        accumulate_t = accumulate_t + similar_table_t
+        show_accuarcate(r, labels_t, train=False)
 
+        if IF_SAVE:
+            np.save(SAVE_NAME,saved_data)
 
-
-            
-
-
-
-
-    for j in range(HIDDEN_UNITS_SIZE):
-        print(j)
-        for i in range(EPOCH):
-            o = get_images_output(brunch_w, images)
-            r = get_similarity_table(o,o)
-            r = get_classfication_score_table(r, labels, accumulate)
-            r = get_loss(r, labels)
